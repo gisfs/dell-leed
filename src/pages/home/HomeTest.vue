c056975ed6853c45ff278b9af5ca546c97daab99
lxs <gisfs@126.com>
2019-12-20 16:18:22 +0800

    temp

--- src/pages/home/HomeTest.vue
+++ src/pages/home/HomeTest.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
-
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
       <span>这是一段信息</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
@@ -14,31 +9,101 @@
       </span>
     </el-dialog>
 
+    <br />
+    <el-popover
+      placement="top-start"
+      title="标题"
+      width="200"
+      trigger="hover"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+    >
+      <el-button style="width:33%" slot="reference">hover 激活</el-button>
+    </el-popover>
+
+    <br />
+    <el-table :data="tableData" style="width: 100%" border>
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+
+    <br>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="日期" width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-watch"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
+
 <script>
 export default {
-  name: 'HomeTest',
+  name: "HomeTest",
   data() {
     return {
-      dialogVisible: false
+      dialogVisible: false,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
     };
   },
   methods: {
     handleClose(done) {
-      this.$confirm('确认关闭？')
+      this.$confirm("确认关闭？")
         .then(_ => {
           done();
         })
         .catch(_ => {});
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
     }
   }
-  
 };
-
 </script>
 
 <style lang='scss' scoped>
-
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,20 @@
 module.exports = {
- lintOnSave: true,
+  lintOnSave: true, // eslint 验证
   
- devServer: {
- proxy: {
-  // proxy all requests starting with /api to jsonplaceholder
-  '/api': {
-  target: 'http://localhost:8080', //代理接口
-  changeOrigin: true,
-  pathRewrite: {
-   '^/api': '/static/mock' //代理的路径
-  }
-  }
- }
- }
+  devServer: {
+    proxy: {
+      // proxy all requests starting with /api to jsonplaceholder
+      '/api': {
+        target: 'http://localhost:8080', //代理接口
+        changeOrigin: true,
+        pathRewrite: {
+        '^/api': '/static/mock' //代理的路径
+        }
+      }
+    },
+    port: 8079,     // 端口
+  },
+
+
+
 }
